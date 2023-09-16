1f7ac3f9bd69b9f7f1f4226391b5e0b20002dbc3
Benjamin Sisson <laura.h.sisson@gmail.com>
2023-09-16 00:03:15 -0400

    Update embed.cpp

--- embed.cpp
+++ embed.cpp
@@ -1,5 +1,5 @@
 //=======================================================================
-// Copyright 2007 Aaron Windsor - Ben Sisson
+// Copyright 2007 Aaron Windsor - Laura Sisson
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
